358c666bd1330bf8b5b2514270817ee0d4f55993
DamruRavihara <damruravihara2@gmail.com>
2021-08-13 12:41:40 +0530

    initial commit

---
+++ app/src/main/java/com/example/mysaved/SaveJobs.java
@@ -0,0 +1,15 @@
+package com.example.mysaved;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SaveJobs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.save_jobs);
+
+    }
+}
