5b02ababe8d0f1cdf6da2d6c1ac4f8efe93847b6
DamruRavihara <damruravihara2@gmail.com>
2021-08-14 13:03:15 +0530

    initial commit

---
+++ app/src/main/java/com/example/mysaved/Request_jobs_home.java
@@ -0,0 +1,14 @@
+package com.example.mysaved;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Request_jobs_home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_jobs_home);
+    }
+}
