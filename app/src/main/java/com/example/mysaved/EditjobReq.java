15588ec82637575bcceebcc54cd47412df4b6d7c
akilakavinda99 <akilakavinda909@gmail.com>
2021-08-19 10:01:13 +0530

    Edit Job name conflict

--- app/src/main/java/com/example/mysaved/EditjobReq.java
+++ app/src/main/java/com/example/mysaved/EditjobReq.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class EditJob extends AppCompatActivity {
+public class EditjobReq extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edit_job);
+        setContentView(R.layout.activity_edit_jobreq);
     }
 }
