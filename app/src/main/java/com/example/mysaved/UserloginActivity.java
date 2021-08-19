aa3260ff1537cc06cdce93c8a487165bbe693a28
vPankaja <vpankaja42@gmail.com>
2021-08-19 09:33:57 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mysaved/UserloginActivity.java
@@ -0,0 +1,18 @@
+package com.example.mysaved;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class UserloginActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_userlogin);
+
+    }
+}
