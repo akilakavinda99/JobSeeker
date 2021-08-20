ca5a66a042dc01fbf1b476b19268af6765f7b920
akilakavinda99 <akilakavinda909@gmail.com>
2021-08-21 02:34:33 +0530

    Register user part done

--- app/src/main/java/com/example/mysaved/Request_jobs_home.java
+++ app/src/main/java/com/example/mysaved/Request_jobs_home.java
@@ -8,14 +8,11 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class Request_jobs_home extends AppCompatActivity {
-    FirebaseDatabase rootNode;
-    DatabaseReference reference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request_jobs_home);
-        rootNode = FirebaseDatabase.getInstance();
-        reference = rootNode.getReference().child("user");
-        reference.setValue("Testing Database");
+
     }
 }
