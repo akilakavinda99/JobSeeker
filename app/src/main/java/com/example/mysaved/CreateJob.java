b5f718ab7cd217574a00246978e0afd7fe2163e8
amilasm <amilasampath1400@gmail.com>
2021-08-20 17:29:20 +0530

    database add for CreateJob

--- app/src/main/java/com/example/mysaved/CreateJob.java
+++ app/src/main/java/com/example/mysaved/CreateJob.java
@@ -3,26 +3,38 @@ package com.example.mysaved;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Spinner;
 
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.annotations.Nullable;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CreateJob extends AppCompatActivity {
 
     EditText company_name, job_title, salary, job_description, email, phone;
-    Spinner type_spinner, district_spinner , city_spinner;;
+    Spinner type_spinner, district_spinner;
     Button create_btn;
     ImageView b1_btn, cimg, addimage;
+    ProgressBar progressBar;
+    Uri imageUri;
 
     FirebaseDatabase rootNode;
     DatabaseReference reference;
+    StorageReference reference2 = FirebaseStorage.getInstance().getReference();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,18 +49,21 @@ public class CreateJob extends AppCompatActivity {
         phone = findViewById(R.id.et_phone);
         type_spinner = (Spinner)findViewById(R.id.et_jobtype);
         district_spinner = (Spinner)findViewById(R.id.et_dis);
-        city_spinner = (Spinner)findViewById(R.id.et_city);
         create_btn = findViewById(R.id.btn_create);
         b1_btn = findViewById(R.id.imageView_b1);
         cimg = findViewById(R.id.imageView_cimg);
         addimage = findViewById(R.id.imageButton_addimage);
+        progressBar = findViewById(R.id.progressBar_cj);
+
 
         create_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+
         rootNode = FirebaseDatabase.getInstance();
         reference = rootNode.getReference("create_job");
+
 
         String name = company_name.getText().toString();
         String title = job_title.getText().toString();
@@ -56,8 +71,10 @@ public class CreateJob extends AppCompatActivity {
         String description = job_description.getText().toString();
         String email1 = email.getText().toString();
         String phone1 = phone.getText().toString();
+        String job_type = type_spinner.getSelectedItem().toString();
+        String district = district_spinner.getSelectedItem().toString();
 
-        JobHelperClass helperClass = new JobHelperClass(name,title,salary1,description,email1,phone1);
+        JobHelperClass helperClass = new JobHelperClass(name,title,salary1,description,email1,phone1,job_type,district);
 
         reference.child(phone1).setValue(helperClass);
 
@@ -66,3 +83,5 @@ public class CreateJob extends AppCompatActivity {
 
     }
 }
+
+
