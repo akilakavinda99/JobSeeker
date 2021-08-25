e834dbfeb7158af3f5108c406b3ef0e19fe4b868
vPankaja <vpankaja42@gmail.com>
2021-08-25 18:31:48 +0530

    String Updated

--- app/src/main/java/com/example/mysaved/Profile.java
+++ app/src/main/java/com/example/mysaved/Profile.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ProfileviewActivity extends AppCompatActivity {
+public class Profile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profileview);
+        setContentView(R.layout.activity_profile);
     }
 }
