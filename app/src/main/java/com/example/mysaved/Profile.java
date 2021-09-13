74779670622162273bf08906a31dfcde767ec2c2
vPankaja <vpankaja42@gmail.com>
2021-09-14 00:44:36 +0530

    update login and profile

--- app/src/main/java/com/example/mysaved/Profile.java
+++ app/src/main/java/com/example/mysaved/Profile.java
@@ -1,14 +1,140 @@
 package com.example.mysaved;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Profile extends AppCompatActivity {
+    TextView text_Pname,text_Pphone,text_Pgender,text_Pdistrict,text_Pemail,user_name,user_phone,user_gender,user_district,user_email;
+    Button edit_user_btn,verify_now_btn;
+    ImageView user_back_btn,verification_badge;
+    FirebaseAuth fAuth;
+    DatabaseReference databaseReference;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        text_Pname = findViewById(R.id.tv_nameP);
+        text_Pphone = findViewById(R.id.tv_mobilenoP);
+        text_Pgender = findViewById(R.id.tv_genderP);
+        text_Pdistrict = findViewById(R.id.tv_districtP);
+        text_Pemail = findViewById(R.id.tv_emailP);
+        user_name =findViewById(R.id.tv_user_name);
+        user_phone =findViewById(R.id.tv_user_Mno);
+        user_gender =findViewById(R.id.tv_user_gender);
+        user_district =findViewById(R.id.tv_user_district);
+        user_email =findViewById(R.id.tv_user_email);
+        edit_user_btn = findViewById(R.id.btn_edit_user);
+        user_back_btn = findViewById(R.id.userprofile_back_btn);
+        verification_badge = findViewById(R.id.verified_badge);
+        verify_now_btn = findViewById(R.id.btn_verify_now);
+
+        //get current instance of firebase authentication
+        fAuth = FirebaseAuth.getInstance();
+
+        //is user is not logged in, return to Login page
+        if (fAuth.getCurrentUser() == null){
+            Intent i = new Intent(getApplicationContext(), UserloginActivity.class).putExtra("from", "profile");
+            startActivity(i);
+        }
+        else{
+            userID = fAuth.getCurrentUser().getUid();
+            FirebaseUser user = fAuth.getCurrentUser();
+
+            //fetch user data from DB
+            databaseReference = FirebaseDatabase.getInstance().getReference().child("user").child(userID);
+            databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    //assign values to text fields
+                    user_name.setText(snapshot.child("Name").getValue().toString());
+                    user_phone.setText(snapshot.child("Phone").getValue().toString());
+                    user_gender.setText(snapshot.child("Gender").getValue().toString());
+                    user_email.setText(snapshot.child("District").getValue().toString());
+                    user_district.setText(snapshot.child("Email").getValue().toString());
+
+                }
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+
+            //check whether user is verified or not
+            Map<String,Object> USER = new HashMap<>();
+            if (user.isEmailVerified()){
+                USER.put("verified","y");
+            }
+            else {
+                USER.put("verified","n");
+            }
+            //update verified in DB
+            DatabaseReference df = FirebaseDatabase.getInstance().getReference().child("user").child(userID);
+            df.updateChildren(USER);
+
+
+            //display verification badge and verify now button
+            if (user.isEmailVerified()){
+                verification_badge.setVisibility(View.VISIBLE);
+            }else {
+                verify_now_btn.setVisibility(View.VISIBLE);
+
+                verify_now_btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        user.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(getApplicationContext(), "Verification Email Has Been Sent", Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+
+                            }
+                        });
+                    }
+                });
+            }
+
+            edit_user_btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(getApplicationContext(), CreateJob.class));
+                }
+            });
+
+        }
+
+        user_back_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), CreateJob.class));
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/mysaved/UserloginActivity.java
+++ app/src/main/java/com/example/mysaved/UserloginActivity.java
@@ -1,11 +1,35 @@
 package com.example.mysaved;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class UserloginActivity extends AppCompatActivity {
+    FirebaseDatabase rootNode;
+    EditText email,password;
+    Button signup,login;
+    TextView fgt_password;
+    FirebaseAuth fAuth;
 
 
 
@@ -13,6 +37,98 @@ public class UserloginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_userlogin);
+
+        email = findViewById(R.id.et_log_email);
+        password = findViewById(R.id.et_log_password);
+        login = findViewById(R.id.btn_login);
+        signup = findViewById(R.id.btn_sign_up);
+        fgt_password = findViewById(R.id.tv_fgt_password);
+
+        //get current instance of firebase authentication
+        fAuth = FirebaseAuth.getInstance();
+
+        rootNode=FirebaseDatabase.getInstance();
+
+
+        // if the user is not registered
+        signup.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            startActivity(new Intent(getApplicationContext(), RegisterUserActivity.class));
+        }
+    });
+
+        // Login to the system when clicks the login button
+        login.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+
+            String text_email = email.getText().toString().trim();
+            String text_password = password.getText().toString().trim();
+
+            //validation
+            if(TextUtils.isEmpty(text_email)){
+                email.setError("Email is required");
+                return;
+            }
+            if(TextUtils.isEmpty(text_password)){
+                password.setError("Password is required");
+                return;
+            }
+
+            if(text_password.length() < 6){
+                password.setError("Password must greater than 6 characters");
+            }
+
+
+            //authenticate user
+            fAuth.signInWithEmailAndPassword(text_email,text_password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        startActivity(new Intent(getApplicationContext(), Profile.class));
+                    }
+                    else {
+                        Toast.makeText(UserloginActivity.this, "Login Unsuccessful", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    });
+
+        fgt_password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText resetmail = new EditText(v.getContext());
+                AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(v.getContext());
+                passwordResetDialog.setTitle("Reset Password");
+                passwordResetDialog.setMessage("Enter your email To receive reset link");
+                passwordResetDialog.setView(resetmail);
+
+                passwordResetDialog.setPositiveButton("Send Link", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //extract email and send email
+                        String mail = resetmail.getText().toString();
+                        fAuth.sendPasswordResetEmail(mail).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(UserloginActivity.this, "Reset link sent to your email", Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(UserloginActivity.this, "Error! Link is not sent", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                    }
+                });
+
+                passwordResetDialog.create().show();
+            }
+        });
 
     }
 }
